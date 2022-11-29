5b15ad0f65c623a757e3ccfdea203b437301a0d0
Kyle Andrus <kyleandrus3@gmail.com>
2022-11-29 15:16:48 -0700

    add event listeners to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import './less/index.less'
 // Your code goes here!
 const navLinks = document.querySelectorAll('.nav-link');
 const logoHead = document.querySelector('.logo-heading');
+const introH2 = document.querySelector('.intro h2');
+const introp = document.querySelector('.intro p');
+const contHome = document.querySelectorAll('.text-content h2')
+const contDest = document.querySelector('.content-destination')
 const logoSpinning = [
     {transform: 'rotate(0) scale(1)'},
     {transform: 'rotate(360deg) scale(0)'}
@@ -31,3 +35,30 @@ logoHead.addEventListener('click', () => {
     logoHead.animate(logoSpinning, logoTiming);
 })
 
+introH2.addEventListener('wheel', evt => {
+    evt.target.style.fontWeight = '900';
+})
+
+introp.addEventListener('dblclick', evt => {
+    evt.target.style.fontSize = '3em';
+})
+
+window.addEventListener('scroll', evt => {
+    contHome.forEach(element => element.style.color = 'red');
+})
+
+window.addEventListener('load', () => {
+    alert("Welcome to the Fun Bus!")
+})
+
+introp.addEventListener('copy', evt => {
+    evt.target.style.color = 'purple';
+})
+
+contDest.addEventListener('mouseenter', evt => {
+    evt.target.style.backgroundColor = 'green';
+})
+
+contDest.addEventListener('mouseleave', evt => {
+    evt.target.style.backgroundColor = 'white';
+})
